f456dc189a2cc547e4508af576c6c76c9002939f
davidaleksejevs <david.aleksejevs@gmail.com>
2022-06-06 12:21:16 +0100

    managed to get routes to work
    after some trouble shooting i managed to get the "articles" route to work, this is where all of my crud stuff will go

--- server.js
+++ server.js
@@ -1,13 +1,15 @@
 const express = require("express");
 const app = express();
 const port = 3000;
+const articelRouter = require("./routes/articles");
+app.use("/articles", articelRouter);
 //
 app.set("view engine", "ejs");
 //
 app.get("/", (req, res) => {
-  res.render("index");
+  res.render("index", { text: "hello" });
 });
 
-app.listen(port, () => {
+app.listen(3000, () => {
   console.log(`Example app listening on port ${port}`);
 });
