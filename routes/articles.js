89081825f96d6d528f4e261b61dfec4b99d6c5a5
davidaleksejevs <david.aleksejevs@gmail.com>
2022-06-07 13:12:51 +0100

    getting new form ready

--- routes/articles.js
+++ routes/articles.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const router = express.Router();
 
-router.get("/", (req, res) => {
-  res.send("In articles");
+router.get("/new", (req, res) => {
+  res.render("articles/new");
 });
 
+router.post("/", (req, res) => {});
 module.exports = router;
