b3bbd2447b800200355840fc0ab142844e31bdcf
davidaleksejevs <david.aleksejevs@gmail.com>
2022-06-14 12:04:33 +0100

    added show page for new articles
    have an active error related to mongoose ?

--- routes/articles.js
+++ routes/articles.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const { findById } = require("./../models/article");
 const router = express.Router();
 const Article = require("./../models/article");
 
@@ -6,10 +7,14 @@ router.get("/new", (req, res) => {
   res.render("articles/new", { article: new Article() });
 });
 
-router.get("/:id", (req, res) => {});
+router.get("/:id", async (req, res) => {
+  const article = findById(req.params.id);
+  if (article == null) res.redirect("/");
+  res.render("articles/show", { article: article });
+});
 
 router.post("/", async (req, res) => {
-  const article = new Article({
+  let article = new Article({
     title: req.body.title,
     description: req.body.description,
     markdown: req.body.markdown,
